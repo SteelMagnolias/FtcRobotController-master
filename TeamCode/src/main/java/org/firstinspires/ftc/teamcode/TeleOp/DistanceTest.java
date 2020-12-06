51e8f8609700c6cf667aa92f8bc48a26c022a13c
SteelMagnolias <steelmagnolias17009@gmail.com>
2020-12-05 20:13:07 -0500

    12/5

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DistanceTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DistanceTest.java
@@ -2,23 +2,34 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@TeleOp(name = "DistanceTest", group = "IterativeOpMode")
-public class DistanceTest extends LinearOpMode {
+@TeleOp(name = "DistanceTest", group = "Iterative OpMode")
+public class DistanceTest extends OpMode {
 
     private DistanceSensor testSensor;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        testSensor = hardwareMap.get(DistanceSensor.class, "sensor_range");
+    public void init() {
+        testSensor = hardwareMap.get(DistanceSensor.class, "sensorTest");
         Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)testSensor;
 
-        telemetry.addData("range", String.format("%.01f mm", testSensor.getDistance(DistanceUnit.MM)));
+        //telemetry.addData("range", String.format("%.01f mm", testSensor.getDistance(DistanceUnit.MM)));
+        //telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        double range = testSensor.getDistance(DistanceUnit.METER);
+        telemetry.addData("range", range);
         telemetry.update();
     }
+
+    @Override
+    public void stop() {}
 }
 
